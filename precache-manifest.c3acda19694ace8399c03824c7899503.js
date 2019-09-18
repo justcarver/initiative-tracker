a23e07ab03b7833917857a043f59a8f2cf9d0c9d
Justin Carver <carver@justcarver.com>
2019-09-18 15:45:37 -0400

    deploy

---
+++ precache-manifest.c3acda19694ace8399c03824c7899503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b66d99b126cb01c1474e",
+    "url": "/initiative-tracker/js/about.be684943.js"
+  },
+  {
+    "revision": "03ab32587c40b8623f50",
+    "url": "/initiative-tracker/css/app.b1da1c57.css"
+  },
+  {
+    "revision": "03ab32587c40b8623f50",
+    "url": "/initiative-tracker/js/app.1d93ddd3.js"
+  },
+  {
+    "revision": "d4ae756f823069250ab4",
+    "url": "/initiative-tracker/js/chunk-vendors.83f0c2fc.js"
+  },
+  {
+    "revision": "634c23cfb5ce07da807859d60567d4a0",
+    "url": "/initiative-tracker/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/initiative-tracker/robots.txt"
+  }
+];
